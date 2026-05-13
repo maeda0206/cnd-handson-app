03d8d2abb442e66ca0c1db610db176f7831bb8bb
maeda <boukenredaksi@gmail.com>
2026-05-13 15:52:11 +0900

    test

--- backend/project/internal/project/handler/project.go
+++ backend/project/internal/project/handler/project.go
@@ -1,16 +1,10 @@
 package handler
 
-import (
-	"string"
-	"time"
-	
-	"context"
+import ("context"
 
 	"github.com/cloudnativedaysjp/cnd-handson-app/backend/project/internal/project/model"
 	"github.com/cloudnativedaysjp/cnd-handson-app/backend/project/internal/project/service"
 	projectpb "github.com/cloudnativedaysjp/cnd-handson-app/backend/project/proto"
-	Name:        name, 
-	Name:        strings.ToLower(name), // bug: 大文字を小文字に変換
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
